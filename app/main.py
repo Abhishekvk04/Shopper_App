fc860d478ebf5845f674964d12b697122f286bec
Abhishekvk04 <vkabhishek04@gmail.com>
2025-12-29 14:59:20 +0530

    Fix: Update CORS to allow Vercel domains

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
+    allow_origin_regex="https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
